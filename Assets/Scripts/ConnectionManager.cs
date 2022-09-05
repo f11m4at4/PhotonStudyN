79107a2d48abea43afaf4a412334bd44cb91171b
Wonseok Park <amurorei2000@gmail.com>
2022-09-05 17:39:34 +0900

    Initial commit

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        //서버 접속 요청
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //마스터 서버 접속성공시 호출(Lobby에 진입할 수 없는 상태)
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+    }
+
+    //마스터 서버 접속성공시 호출(Lobby에 진입할 수 있는 상태)
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        //로비 진입 요청
+        PhotonNetwork.JoinLobby();
+    }
+
+    //로비 진입 성공시 호출
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        //LobbyScene으로 이동
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
